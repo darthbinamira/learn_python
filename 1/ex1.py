2dd603f42bb2058a4c4ece79f392b8376d12f658
Darth Binamira <dummymael@gmail.com>
2015-05-05 00:10:36 +0800

    Exercise 1 - let's do some random printing! :D

---
+++ 1/ex1.py
@@ -0,0 +1,23 @@
+# Exercise 1 - let's do some random printing! :D
+
+import random
+
+line_num = random.randint(1, 7)
+
+if line_num == 1:
+    print "Hello, World!"
+elif line_num == 2:
+    print "Hello again!"
+elif line_num == 3:
+    print "I like typing this."
+elif line_num == 4:
+    print "This is fun."
+elif line_num == 5:
+    print 'Yay! Printing.'
+elif line_num == 6:
+    print "I'd much rather you 'not'."
+elif line_num == 7:
+    print 'I "said" do not touch this.'
+else:
+    print 'How did I get here?'
+
